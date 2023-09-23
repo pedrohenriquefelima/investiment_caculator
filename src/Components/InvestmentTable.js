9e91bd071b91a83ffeabe97931d4bca8b4e2466a
Pedro Lima <pedrohenriquefelima@gmail.com>
2023-09-23 19:10:49 +0100

    formatter

--- src/Components/InvestmentTable.js
+++ src/Components/InvestmentTable.js
@@ -2,14 +2,23 @@ import React from "react";
 import styles from './InvestmentTable.module.css'
 
 const InvestmentTable = (props) => {
+
+
+    const formatter = new Intl.NumberFormat('en-UK', {
+        style: 'currency',
+        currency: 'GBP',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    })
+
     const itemsAll = props.dataTable.map((item) => {
         return (
                 <tr key={Math.random(1000)}>
                     <td>{item.year}</td>
-                    <td>{item.savingsEndOfYear}</td>
+                    <td>{formatter.format(item.savingsEndOfYear)}</td>
                     <td>{item.yearlyInterest}</td>
                     <td>{item.savingsEndOfYear - item.currentSavings - item.yearlyContribution * item.year}</td>
-                    <td>{item.currentSavings + item.yearlyContribution * item.year}</td>
+                    <td>{formatter.format(item.currentSavings + item.yearlyContribution * item.year)}</td>
                 </tr>)
     })
     return (
